f96ed016842f7a754cd46e3b072a7de4c4df9eda
Alexander Williams <alexanderwilliams52@gmail.com>
2021-04-21 16:48:43 -0400

    finished anonymous posting board

--- routes/mongo.js
+++ routes/mongo.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 var path = require('path')
 var express = require('express');
 var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
 
 /* Makes a new post */
 router.post('/new', async (req, res, next) => {
   let client = req.client;
   try {
-    console.log(req.body);
     let insert = await insertPost(client, process.env.DB, "posts", {
       "url": req.body.url,
       "desc": req.body.desc,
@@ -19,6 +19,31 @@ router.post('/new', async (req, res, next) => {
   }
 });
 
+router.get('/all', async (req, res, next) => {
+  let client = req.client;
+  try {
+    let data = await readCollection(client, process.env.DB, "posts", {})
+    res.send({"posts" : data});
+  } catch (e) {
+    next(e)
+  }
+});
+
+router.post('/addComment', async (req, res, next) => {
+  let client = req.client;
+  console.log(req.body);
+  try {
+    let id = req.body.imgId;
+    let comment = req.body.comment;
+    let update = await findAndUpdate(client, process.env.DB, "posts", id,
+                                    {"$push" : {"comments": comment}})
+    res.redirect('/');
+  } catch (e) {
+    next(e)
+  }
+});
+
+
 // Helper: Inserts one post into the Mongo Database
 async function insertPost(client, database, collection, post) {
   try{
@@ -28,4 +53,19 @@ async function insertPost(client, database, collection, post) {
   }
 }
 
+// Helper: Grabs all docs from a collection given a query
+async function readCollection(client, database, collection, search) {
+  const cursor = await client.db(database).collection(collection).find(search)
+  const results = await cursor.toArray();
+  return results;
+}
+
+async function findAndUpdate(client, database, collection, id, update) {
+  try{
+      const result = await client.db(database).collection(collection)
+                           .findOneAndUpdate({"_id" : ObjectID(id)}, update);
+  } catch (e) {
+      console.error(e)
+  }
+}
 module.exports = router;
